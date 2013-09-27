cbd3b5313a759fcf2c292499c288083c0c8ab37d
jaburns <jaburns@jaburns-laptop.(none)>
2013-09-27 00:54:36 -0600

    Removed longish define

--- small.c
+++ small.c
@@ -1,6 +1,6 @@
 #include <stdio.h>// Idiomatic C version of CW's GoL :P ///
-#define cf(a,x)  (((a<<1)&x)!=0)+((a&x)!=0)+(((a>>1)&x)!=0)
-#define Z(a,b,c,x) cf(a,x)+cf(b,x)+cf(c,x)/////////////////
+#define l(a,x) (((a<<1)&x)!=0)+((a&x)!=0)+(((a>>1)&x)!=0)//
+#define Z(a,b,c,x) l(a,x)+l(b,x)+l(c,x)////////////////////
 #define M printf// This is surely too long of a program ///
 typedef unsigned int U;main(){U Q[22],W[22];U*E,*R,i,x,y;E=
 &Q;R=&W;E[0]=R[0]=E[21]=R[21]=0;for(i=1;i<21;i++)E[i]=(rand
