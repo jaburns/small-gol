8c3ca2bc2594b4b03297056e8132fab831e81f38
Jeremy Burns <jeremyaburns@gmail.com>
2013-09-28 13:49:08 -0600

    Even smaller

--- small.c
+++ small.c
@@ -1,14 +1,8 @@
-
-#include <stdio.h>
 #define M printf
-#define l(a) !!(a<<1&x)+!!(a&x)+!!(a>>1&x)
+#include <stdio.h>
 #define F(n)for(n=1;n<33;n++)
-typedef unsigned int U;
-main(){U Q[34],W[34],*E=&Q,*R=&W,i,x,y
-;*E=*R=E[33]=R[33]=0;F(i)E[i]=(rand()+
-(rand()<<16));B:F(y){R[y]=0;F(i){U x=1
-<<(i-1);U S=E[y]&x;U A=l(E[y-1])+l(E[y
-])+l(E[y+1]);if(A==3||S&&A==4){R[y]|=x
-;M("()");}else{M("  ");}}M("\n");}
-fflush(stdout);U*a=E;E=R;R=a;system(
-"sleep 0.1;clear");goto B;}
+#define l(a) !!(a<<1&x)+!!(a&x)+!!(a>>1&x)
+typedef int U;main(){U Q[34],W[34],*E=&Q,*R=&W,i,x,y;*E=*R=E[33]=R[33]=0;F(i)E[i
+]=rand()+(rand()<<16);B:F(y){R[y]=0;F(i){U x=1<<(i-1),S=E[y]&x,A=l(E[y-1])+l(E[y
+])+l(E[y+1]),q=A==3||S&&A==4;R[y]|=q?x:0;M(q?"()":"  ");};M("\n");}fflush(stdout
+);U*a=E;E=R;R=a;system("sleep 0.1;clear");goto B;}
