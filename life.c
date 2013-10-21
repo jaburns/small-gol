55fce8566917e3b600b8fd2b41be94c19b4faa8a
jaburns <jaburns@jaburns-laptop.(none)>
2013-10-20 22:33:34 -0600

    Up to 338, but added srand so it actually changes between runs

--- life.c
+++ life.c
@@ -1,6 +1,6 @@
 #define F(n)for(n=1;n<33;n++)
 #define l(a)+!(2*a&x)+!(a&x)+!(a>>1&x)
-main(){int*a,Q[34],W[34],*E=Q,*R=W,i,y;*E=*Q=E[33]=R[33]=0;F(i)E[i]=rand()+(rand
-()<<16);B:F(y){R[y]=0;F(i){int x=1<<(i-1),A=l(E[y-1])l(E[y])l(E[y+1]),q=A==6||E[
-y]&x&&A==5;R[y]|=q?x:0;printf(q?"[]":"  ");}puts("");}a=E;E=R;R=a;system(
+main(){srand(time(0));int*a,Q[34],W[34],*E=Q,*R=W,i,y;*E=*Q=E[33]=R[33]=0;F(i)E[
+i]=rand()<<8;B:F(y){R[y]=0;F(i){int x=1<<(i-1),A=l(E[y-1])l(E[y])l(E[y+1]),q=A==
+6||E[y]&x&&A==5;R[y]|=q?x:0;printf(q?"[]":"  ");}puts("");}a=E;E=R;R=a;system(
 "sleep 0.1;clear");goto B;}
