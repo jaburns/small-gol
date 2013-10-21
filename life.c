b7abf3a9fc6bc2425dd5f8a5f60789da80438d61
Jeremy Burns <jeremyaburns@gmail.com>
2013-10-21 12:58:26 -0600

    Fixed misnamed variable

--- life.c
+++ life.c
@@ -2,4 +2,4 @@
 #define l(a)+!(2*a&x)+!(a&x)+!(a>>1&x)
 int Q[68],*E=Q,*R=Q+34,y,i;main(){srand(time(0));F(i)E[i]=rand()<<8;B:F(y){R[y]=
 0;F(i){int x=1<<(i-1),A=l(E[y-1])l(E[y])l(E[y+1]),q=A==6||E[y]&x&&A==5;R[y]|=q?x
-:0;printf(q?"[]":"  ");}puts("");}a=E;E=R;R=a;system("sleep .1;clear");goto B;}
+:0;printf(q?"[]":"  ");}puts("");}i=E;E=R;R=i;system("sleep .1;clear");goto B;}
